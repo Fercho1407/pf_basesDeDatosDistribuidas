b7dc28c900bb35631d1dde5266c37f66e42c7019
Fernando Robles <luis.fer1407@gmail.com>
2026-04-09 00:17:18 -0600

    Todo funcionadno correctamente

--- uam/src/main/java/bdd/uam/mx/DTO/LoginRequestDTO.java
+++ uam/src/main/java/bdd/uam/mx/DTO/LoginRequestDTO.java
@@ -1,10 +1,12 @@
 package bdd.uam.mx.DTO;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
 public class LoginRequestDTO {
     private String username;
     private String password;
